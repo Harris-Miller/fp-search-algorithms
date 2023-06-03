6913339b15d28e4b4bb074d1325713bcdc44f82d
harris-miller <harrismillerconsulting@gmail.com>
2023-06-03 17:06:58 -0600

    utility funcs

--- src/fp.ts
+++ src/fp.ts
@@ -4,3 +4,14 @@ export const fst = <A, B>(tuple: [A, B]): A => tuple[0];
 export const snd = <A, B>(tuple: [A, B]): B => tuple[1];
 export const head = <T>(list: T[]): T | undefined => list[0];
 export const last = <T>(list: T[]): T | undefined => list[list.length - 1];
+export const tail = <T>(list: T[]): T[] => Array.prototype.slice.call(list, 1, Infinity);
+export const zipWith = <A, B, C>(fn: (a: A, b: B) => C, as: A[], bs: B[]): C[] => {
+  const len = Math.min(as.length, bs.length);
+  const cs = Array(len);
+  let i = 0;
+  while (i < len) {
+    cs[i] = fn(as[i], bs[i]);
+    i += 1;
+  }
+  return cs;
+};
--- src/utility.ts
+++ src/utility.ts
@@ -1,2 +1,9 @@
-// TODO
-export {};
+import { tail, zipWith } from './fp';
+
+export const incrementalCost = <T>(cost: (a: T, b: T) => number, states: T[]): number[] =>
+  zipWith(cost, states, tail(states));
+
+export const pruning =
+  <T>(next: (a: T) => T[], predicate: (a: T) => boolean) =>
+  (a: T) =>
+    next(a).filter(x => !predicate(x));

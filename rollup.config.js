84fbeb084efad0d21a85872be6fb4ca3129b442d
harris-miller <harrismillerconsulting@gmail.com>
2024-09-26 07:40:04 -0600

    Forget separate exports, just do one file, it's easier and this repo isn't that large

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,9 @@ import typescript from '@rollup/plugin-typescript';
 const output = format => {
   const extension = format === 'esm' ? '.js' : '.cjs';
   return {
-    chunkFileNames: `[name]${extension}`,
     dir: './dist',
     entryFileNames: `[name]${extension}`,
     esModule: format !== 'esm',
-    exports: 'named',
     format,
     sourcemap: true,
   };
@@ -19,10 +17,6 @@ const output = format => {
 const rollupConfig = {
   external: /node_modules/,
   input: {
-    aStar: 'src/aStar/aStar.ts',
-    breadthFirst: 'src/breadthFirst/breadthFirst.ts',
-    depthFirst: 'src/depthFirst/depthFirst.ts',
-    dijkstra: 'src/dijkstra/dijkstra.ts',
     index: 'src/index.ts',
   },
   output: [output('esm'), output('cjs')],

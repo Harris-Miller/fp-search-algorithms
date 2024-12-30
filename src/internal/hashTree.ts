48474436c2f66888849c05a922931636db6fb056
harris-miller <harrismillerconsulting@gmail.com>
2024-12-30 13:02:13 -0700

    Release 0.7.0

--- src/internal/hashTree.ts
+++ src/internal/hashTree.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-/* eslint-disable line-comment-position */
 /* eslint-disable complexity */
 /* eslint-disable no-bitwise */
 /* eslint-disable no-plusplus */
